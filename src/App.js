0bb09ff31f54b97c341a886ab705e9fbae9b9409
tnantoka <tnantoka@bornneet.com>
2016-11-02 23:46:30 +0900

    Add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Nav, NavItem, Navbar } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Navbar>
+          <Navbar.Header>
+            <Navbar.Brand>
+              <a href="/">Coffeed</a>
+            </Navbar.Brand>
+          </Navbar.Header>
+          <Nav pullRight>
+            <NavItem eventKey={1} href="https://github.com/tnantoka/coffeed">GitHub</NavItem>
+          </Nav>
+        </Navbar>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
